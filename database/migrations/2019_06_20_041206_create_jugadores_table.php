df1f537f470bc3d87b1f79c098dd151b4ee70422
Alejok <Alejok>
2019-06-19 23:28:14 -0500

    migraciones

---
+++ database/migrations/2019_06_20_041206_create_jugadores_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJugadoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jugadores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->unsignedInteger('posicion_id');
+            $table->integer('numero');
+            $table->string('foto');
+            $table->unsignedInteger('equipo_id');
+            $table->foreign('posicion_id')->references('id')->on('posiciones');
+            $table->foreign('equipo_id')->references('id')->on('equipos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jugadores');
+    }
+}
